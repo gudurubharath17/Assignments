162878bf6d826a7420de80b53b21bea1014d7f85
bharath kumar <gudurubharath93@gmail.com>
2026-01-18 19:04:30 +0530

    assignment1 name updated without spaces

---
+++ Assignments/Assignment-1-Datatypes.js
@@ -0,0 +1,88 @@
+//Assignment 1
+
+//Temperature of a city in degrees Celsius: 25.5
+let citytemperature = 25.5;
+console.log("citytemperature:" +citytemperature); //output: citytemperature:25.5
+console.log("datatype of citytemperature:" +typeof citytemperature); //output: datatype of citytemperature:number
+
+//Whether a customer has placed an order: true or false
+let isorderplaced = true;
+console.log(isorderplaced); //output: true
+console.log(typeof isorderplaced); //output: boolean
+
+//Person's phone number: "123-456-7890"
+let phonenumber = "123-456-7890";
+console.log("person's phone number: " +phonenumber); //output: person's phone number: 123-456-7890
+console.log(typeof phonenumber); //output: string
+
+//Amount of money in a customer's bank account: 1000.50
+let money =1000.50;
+console.log("Amount of money: " +money); //output: Amount of money: 1000.5
+console.log(typeof money); //output: number
+
+//Person's email address: "john.doe@example.com"
+let emailid = "john.doe@example.com";
+console.log(typeof emailid); //output: string
+
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let latitude = 37.7749;
+let longitude =-122.4194;
+console.log("Coordinates of a location (latitude, longitude): " +latitude+", "+longitude+ "and datatyepe is "+typeof latitude);
+//output:Coordinates of a location (latitude, longitude): 37.7749, -122.4194and datatyepe is number
+
+//Person's marital status: true or false
+let ismarried = false;
+console.log(typeof ismarried); //output: boolean
+
+//Person's occupation: "Software Engineer"
+let occupation = "Software Engineer";
+console.log(typeof occupation); //output: string
+
+//Person's favourite colour: "Blue"
+let fav_color = "Blue";
+console.log(typeof fav_color); //output: string
+
+//Current year: 2023
+let year = 2023;
+console.log(typeof year); //output: number
+
+//Number of followers on a social media platform: 1,000,000
+let no_of_followers= 1000000;
+console.log(typeof no_of_followers); //output: number
+
+//Rating of a movie: 7.5
+let movie_rating= 7.5
+console.log(typeof movie_rating); //output: number
+
+//Person's blood type: 'A'
+let blood_type='A';
+console.log(typeof blood_type); //output: string
+
+//Title of a book: "To Kill a Mockingbird"
+let title_of_book="To Kill a Mockingbird";
+console.log(typeof title_of_book); //output: string
+
+//Number of employees in a company: 500
+let no_of_employees= 500;
+console.log(typeof no_of_employees); //output: number
+
+//Time of an event: 2:30 PM
+let time= "2:30 PM";
+console.log(typeof time); //output: string
+
+//Name of a country: "United States"
+let country= "United States";
+console.log(typeof country); //output: string
+
+//Person's eye color: "Brown"
+let eye_color = "Brown";
+console.log(typeof eye_color); //output: string
+
+//Person's birthplace: "New York City"
+let birthplace= "New York City";
+console.log(typeof birthplace); //output: string
+
+//Distance between two cities: 200.5
+let distance= 200.5
+console.log(distance); //output: 200.5
+console.log(typeof distance); //output: number
